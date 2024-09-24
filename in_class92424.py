b74a8b3efabf46433ccb8cbadfbdbd6003b08f67
DiegoGA05 <dagaray2@miners.utep.edu>
2024-09-24 10:36:50 -0600

    In-class examples

---
+++ in_class92424.py
@@ -0,0 +1,30 @@
+### Queries
+import pandas as pd
+
+filename = './big-mac-full-index.csv'
+df = pd.read_csv(filename)
+
+print(df['dollar_price'])
+
+query = f"(iso_a3 == 'MEX')"
+mxn_df = df.query(query)
+
+### Various print queries
+
+# print(mxn_df['dollar_price'].min())
+# print(mxn_df['dollar_price'].max())
+# print(round(mxn_df['dollar_price'].mean(),2))
+# print(mxn_df)
+# print(mxn_df.median())
+# print(type(mxn_df['dollar_price'].mean(),2))
+
+### Row Queries
+
+# query = f"(iso_a3 == 'JPN')"
+# jpn_df = df.query(query)
+# min_idx = jpn_df['dollar_price'].idxmin()
+# # min_idx = jpn_df['dollar_price'].idxmax()
+
+# print(min_idx)
+# print(jpn_df.loc[min_idx])
+# print(jpn_df.loc[min_idx]['dollar_price'])
